f4a7306d762db7962669d5521da6691fb6badf50
Jacob <cob.mossy@gmail.com>
2024-04-28 17:13:52 +0100

    separate out infer_cluster_label_indices

--- pyslingshot/util.py
+++ pyslingshot/util.py
@@ -19,8 +19,25 @@ def isint(x):
     else:
         return a == b
 
+
 def isstr(x):
     return isinstance(x, str)
 
+
 def scale_to_range(x, a=0, b=1):
     return ((x - x.min()) / (x.max() - x.min())) * (b - a) + a
+
+
+def infer_cluster_label_indices(cluster_labels):
+    """
+    Infers the cluster label indices: a vector of
+    :param cluster_labels: list of string labels or list of integers denoting cluster assignment
+    :return: np.array of cluster assignments
+    """
+    if isint(cluster_labels[0]):
+        return cluster_labels
+    elif isstr(cluster_labels[0]):
+        # Convert list of str labels into a list of int indices
+        return np.array([np.where(np.unique(cluster_labels) == label)[0][0] for label in cluster_labels])
+    else:
+        raise ValueError("Unexpected cluster label dtype.")
